3a53d5f68c228adea29d002aeb0b1ae6ba84c5ac
Gabriel Marques <gabriel.marques@hublocal.com.br>
2023-10-25 20:54:43 -0300

    add dockerfile and docker-compose.yml

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   dev1: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'database.db'),
+      filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
     migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations'),
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
     },
     useNullAsDefault: true,
   },
   dev2: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
@@ -21,8 +27,14 @@ module.exports = {
       password: process.env.DB_PASSWORD,
     },
     migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations'),
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
     },
     useNullAsDefault: true,
-  }
+  },
 };
--- src/database/knex/index.js
+++ src/database/knex/index.js
@@ -1,9 +1,9 @@
-const config = require('../../../knexfile');
-const knex = require('knex');
+const config = require("../../../knexfile");
+const knex = require("knex");
 
 const msgs = {
-  dev1: 'Using Sqlite',
-  dev2: 'Using Mysql',
+  dev1: "Using Sqlite",
+  dev2: "Using Mysql",
 };
 
 console.log(`*** ${msgs[process.env.NODE_ENV]} ***`);
